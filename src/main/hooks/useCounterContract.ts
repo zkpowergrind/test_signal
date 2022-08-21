9ff344ddaecee3e66822630eb5cde4b40801e2e0
zkpowergrind <zkpowergrind@gmail.com>
2022-08-21 09:24:39 -0700

    udpated contract with drawer data appeneded to input

--- src/main/hooks/useCounterContract.ts
+++ src/main/hooks/useCounterContract.ts
@@ -10,7 +10,7 @@ export function useCounterContract() {
     address:
       //"0x05d8a0ddef7d81623f3adb030d2338a26f79c2eb3d4ec322b43041cd5417f58e",
       //"0x05fce2b74494bfe7da1e7f49e55f5edd3a4473545bafccf6923548ad01727efe",
-      "0x0012bd40ead0cdb58013a70eaf4a44df8961b347aaee7ff54256bab278a3edf5",
+      "0x07761e97b4f17d93064fa32019d1193e735da62d07964512d54fa3d76777c478",
   })
 }
 
